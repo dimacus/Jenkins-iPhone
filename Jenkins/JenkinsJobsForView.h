d4057bb622562dfc1efa4641720c5fef71795baf
Dima Kovalenko <dima@groupon.com>
2012-01-31 14:02:34 -0500

    Adding initial JenkinsJobsForView controller

---
+++ Jenkins/JenkinsJobsForView.h
@@ -0,0 +1,16 @@
+//
+//  JenkinsJobsForView.h
+//  Jenkins
+//
+//  Created by Dima Kovalenko on 1/31/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface JenkinsJobsForView : UITableViewController {
+    
+}
+
+@end
