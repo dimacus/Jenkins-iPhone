22ff0458446e949c04ace3a759315d87578b7fbf
Dima Kovalenko <dima@groupon.com>
2012-02-02 15:42:27 -0500

    Adding Job view

---
+++ Jenkins/JenkinsJobView.h
@@ -0,0 +1,16 @@
+//
+//  JenkinsJobView.h
+//  Jenkins
+//
+//  Created by Dima Kovalenko on 2/2/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface JenkinsJobView : UIViewController {
+    
+}
+
+@end
