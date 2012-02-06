f03178deabc471f7ee5eba54bfcf192e2946bb51
Dima Kovalenko <dima@groupon.com>
2012-02-06 13:21:13 -0500

    Setting up the build history view

--- Jenkins/JenkinsBuildHistoryView.h
+++ Jenkins/JenkinsBuildHistoryView.h
@@ -11,6 +11,9 @@
 
 @interface JenkinsBuildHistoryView : UIViewController <UITableViewDelegate, UITableViewDataSource, NSFetchedResultsControllerDelegate>
 
-
+@property (nonatomic, retain) IBOutlet UITableView* JenkinsBuildHistoryTable;
+@property (nonatomic, retain) NSMutableArray* InQueueData;
+@property (nonatomic, retain) NSMutableArray* BuildingData;
+@property (nonatomic, retain) NSMutableArray* FinishedData;
 
 @end
--- Jenkins/JenkinsBuildHistoryView.m
+++ Jenkins/JenkinsBuildHistoryView.m
@@ -11,17 +11,19 @@
 
 @implementation JenkinsBuildHistoryView
 
-- (id)initWithStyle:(UITableViewStyle)style
-{
-    self = [super initWithStyle:style];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
+@synthesize JenkinsBuildHistoryTable;
+@synthesize InQueueData;
+@synthesize BuildingData;
+@synthesize FinishedData;
+
+
 
 - (void)dealloc
 {
+    [JenkinsBuildHistoryTable release];
+    [BuildingData release];
+    [FinishedData release];
+    [InQueueData release];
     [super dealloc];
 }
 
@@ -38,12 +40,10 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    InQueueData  = [[NSMutableArray alloc] initWithObjects:@"Loading queue...", nil];
+    BuildingData = [[NSMutableArray alloc] initWithObjects:@"Loading in progress builds...", nil];
+    FinishedData = [[NSMutableArray alloc] initWithObjects:@"Loading finished builds...", nil];
 }
 
 - (void)viewDidUnload
@@ -83,17 +83,44 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
+    return 3;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+    switch (section) {
+        case 0:
+            return [InQueueData count];
+            break;
+        case 1:
+            return [BuildingData count];
+        case 2:
+            return [FinishedData count];
+        default:
+            return 1; //Just in case, but this should never be
+            break;
+    }
 }
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    switch (section) {
+        case 0:
+            return @"In Queue";
+            break;
+            
+        case 1:
+            return @"Building";
+            break;
+            
+        case 2:
+            return @"Finished";
+            break;
+        default:
+            break;
+    }
+    return nil;
+}
+
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
@@ -104,49 +131,22 @@
         cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
     }
     
-    // Configure the cell...
+    switch (indexPath.section) {
+        case 0:
+            cell.textLabel.text = [InQueueData objectAtIndex:indexPath.row];
+            break;
+        case 1:
+            cell.textLabel.text = [BuildingData objectAtIndex:indexPath.row];
+            break;
+        case 2:
+            cell.textLabel.text = [FinishedData objectAtIndex:indexPath.row];
+            break;            
+        default:
+            break;
+    }
     
     return cell;
 }
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
 #pragma mark - Table view delegate
 
